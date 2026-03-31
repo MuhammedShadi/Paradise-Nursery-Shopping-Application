3b78833bd74fc50ef76787494c8f1f0be2d70a17
Muhammed Shadi <muhammed.samir.abushadi@gmail.com>
2026-03-31 16:02:17 +0300

    Update AboutUs.jsx

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,1 +1,17 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is your one-stop destination for beautiful and healthy houseplants.
+        We provide a wide variety of indoor and outdoor plants to bring nature closer to your home.
+      </p>
+      <p>
+        Our mission is to make plant care simple and enjoyable for everyone, whether you're a beginner
+        or an experienced gardener.
+      </p>
+    </div>
+  );
+}
 
+export default AboutUs;
